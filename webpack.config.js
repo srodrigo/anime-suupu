b0ed2cf0288f1b56e79c05314a6ef5956c0a3aaa
Sergio Rodrigo Royo <sergio.rodrigo.royo@gmail.com>
2020-08-06 21:47:40 +0100

    ignore lint rules on webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import path from "path";
 import webpack from "webpack";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 
 module.exports = {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
   entry: path.resolve(__dirname, "src", "index.tsx"),
   output: {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
     path: path.join(__dirname, "dist"),
     filename: "bundle.js",
   },
@@ -31,6 +32,5 @@ module.exports = {
       },
     ],
   },
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
   plugins: [new HtmlWebpackPlugin(), new webpack.NamedModulesPlugin()],
 };

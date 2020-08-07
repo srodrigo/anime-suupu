d3e9ca1bb3c373ebb128202621e7c1fcb1fe560f
Sergio Rodrigo Royo <sergio.rodrigo.royo@gmail.com>
2020-08-07 10:41:42 +0100

    fix build
    It was broken due to missing @bable/register dependency. Adding it,
    fixed the problem when using 'import'.

--- webpack.config.babel.ts
+++ webpack.config.babel.ts
@@ -1,9 +1,7 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import path from "path";
 import webpack from "webpack";
 import HtmlWebpackPlugin from "html-webpack-plugin";
+import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
 
 module.exports = {
   entry: path.resolve(__dirname, "src", "index.tsx"),
@@ -16,21 +14,17 @@ module.exports = {
     alias: {
       "react-dom": "@hot-loader/react-dom",
     },
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: [".tsx", ".ts", "jsx", ".js"],
   },
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx|ts|tsx)$/,
         use: "babel-loader",
-        exclude: /node_modules|packages/,
-      },
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
         exclude: /node_modules/,
       },
     ],
   },
-  plugins: [new HtmlWebpackPlugin(), new webpack.NamedModulesPlugin()],
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  plugins: [new HtmlWebpackPlugin(), new webpack.NamedModulesPlugin(), new BundleAnalyzerPlugin()],
 };

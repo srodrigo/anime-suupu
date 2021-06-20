0eb1492d1ba1fbb4661156ea1c901bd5d7b3352c
Sergio Rodrigo Royo <sergio.rodrigo.royo@gmail.com>
2021-06-20 13:20:38 +0100

    ci: block pr if coverage is not enough

--- jest.config.js
+++ jest.config.js
@@ -9,4 +9,12 @@ module.exports = {
   setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
   collectCoverage: true,
   coverageDirectory: ".coverage",
+  coverageThreshold: {
+    global: {
+      branches: 95,
+      functions: 95,
+      lines: 95,
+      statements: 95,
+    },
+  },
 };

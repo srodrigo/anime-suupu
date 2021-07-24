951e96171e9c60edb048653f44fd112bb62c0e29
Sergio Rodrigo Royo <sergio.rodrigo.royo@gmail.com>
2021-07-24 09:55:50 +0100

    feat(#136): show series images

--- src/series/TrendingSeries.tsx
+++ src/series/TrendingSeries.tsx
@@ -12,14 +12,19 @@ type SeriesListProperties = {
   series: CollectionItem[] | null;
 };
 
-const SeriesList = ({ series }: SeriesListProperties): JSX.Element | null =>
-  series && (
-    <ul data-testid="series-list">
-      {series.map((item) => (
-        <li key={item.id}>{item.attributes.titles.en || item.attributes.titles.en_us}</li>
-      ))}
-    </ul>
+const buildSeriesItem = (item: CollectionItem): JSX.Element => {
+  const title = item.attributes.titles.en || item.attributes.titles.en_us;
+
+  return (
+    <li key={item.id}>
+      <h2>{title}</h2>
+      <img alt={title} src={item.attributes.posterImage.small} />
+    </li>
   );
+};
+
+const SeriesList = ({ series }: SeriesListProperties): JSX.Element | null =>
+  series && <ul data-testid="series-list">{series.map(buildSeriesItem)}</ul>;
 
 const TrendingSeries = (): JSX.Element | null => {
   const [series, setSeries] = useState<CollectionItem[] | null>(null);

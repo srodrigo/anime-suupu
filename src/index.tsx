2bee1fc6c184206a450b8bf6a42b017acb466095
Sergio Rodrigo Royo <sergio.rodrigo.royo@gmail.com>
2021-07-26 08:50:26 +0100

    feat: remove unnecessary root element
    The root was attached to a newly created dom element instead of to the
    existing div on the index.html

--- src/index.tsx
+++ src/index.tsx
@@ -7,13 +7,9 @@ import packageInfo from "../package.json";
 
 import "./styles/main.scss";
 
-const root = document.createElement("div");
-document.body.append(root);
-
-// eslint-disable-next-line @typescript-eslint/no-unsafe-call
 render(
   <AppContext.Provider value={{ version: packageInfo.version }}>
     <Root />
   </AppContext.Provider>,
-  root
+  document.querySelector("#root")
 );

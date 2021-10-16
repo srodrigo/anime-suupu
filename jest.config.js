af95c5bcc86aec51790745c9fe30083cc86795f6
Sergio Rodrigo Royo <sergio.rodrigo.royo@gmail.com>
2021-10-16 11:24:10 +0200

    chore(#223): remove unused babel deps

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,5 @@
 module.exports = {
   roots: ["<rootDir>/src"],
-  transform: {
-    "^.+\\.[t|j]sx?$": "babel-jest",
-  },
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.[t|j]sx?$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   testEnvironment: "jsdom",
